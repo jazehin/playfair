6099b5926210add19d62dfd5708448d2078be0dd
kiss.bence2 <kiss.bence2@28-05>
2023-04-27 09:39:07 +0200

    Add project files.

---
+++ playfair/PlayfairKodolo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace playfair
+{
+    internal class PlayfairKodolo
+    {
+        private char[,] matrix = new char[5,5];
+        public PlayfairKodolo(string allomanynev)
+        {
+            using StreamReader sr = new(allomanynev);
+            string szoveg = sr.ReadToEnd().Replace("\r\n", "");
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = szoveg[i * 5 + j];
+                }
+            }
+        }
+
+        public int SorIndex(char keresett)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (keresett == matrix[i, j]) return i;
+                }
+            }
+            return -1;
+        }
+
+        public int OszlopIndex(char keresett)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (keresett == matrix[i, j]) return j;
+                }
+            }
+            return -1;
+        }
+
+        public string KodolBetupar(string betupar)
+        {
+            //0: x1
+            //1: y1
+            //2: x2
+            //3: y2
+            int[] indexek = { SorIndex(betupar[0]), OszlopIndex(betupar[0]), SorIndex(betupar[1]), OszlopIndex(betupar[1]) };
+            int[] behelyettesitendoIndexek = new int[4];
+
+            //a
+            if (indexek[0] == indexek[2])
+            {
+                behelyettesitendoIndexek[0] = indexek[0];
+                behelyettesitendoIndexek[1] = indexek[1] < 4 ? indexek[1] + 1 : 0;
+                behelyettesitendoIndexek[2] = indexek[2];
+                behelyettesitendoIndexek[3] = indexek[3] < 4 ? indexek[3] + 1 : 0;
+            }
+
+            //b
+            else if (indexek[1] == indexek[3])
+            {
+                behelyettesitendoIndexek[0] = indexek[0] < 4 ? indexek[0] + 1 : 0;
+                behelyettesitendoIndexek[1] = indexek[1];
+                behelyettesitendoIndexek[2] = indexek[2] < 4 ? indexek[2] + 1 : 0;
+                behelyettesitendoIndexek[3] = indexek[3];
+            }
+
+            //c
+            else
+            {
+                behelyettesitendoIndexek[0] = indexek[0];
+                behelyettesitendoIndexek[1] = indexek[3];
+                behelyettesitendoIndexek[2] = indexek[2];
+                behelyettesitendoIndexek[3] = indexek[1];
+            }
+
+            return $"{matrix[behelyettesitendoIndexek[0], behelyettesitendoIndexek[1]]}{matrix[behelyettesitendoIndexek[2], behelyettesitendoIndexek[3]]}";
+        }
+    }
+}
---
+++ playfair/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace playfair
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            PlayfairKodolo kodolo = new PlayfairKodolo(@"..\..\..\..\src\kulcstabla.txt");
+
+            Console.Write("6. feladat - Kérek egy nagybetűt: ");
+            char karakter = Console.ReadLine()[0];
+            Console.WriteLine($"A karakter sorának az indexe: {kodolo.SorIndex(karakter)}");
+            Console.WriteLine($"A karakter oszlopának az indexe: {kodolo.OszlopIndex(karakter)}");
+
+            Console.Write("8. feladat - Kérek egy karakterpárt: ");
+            string karakterpar = Console.ReadLine();
+            string kodoltpar = kodolo.KodolBetupar(karakterpar);
+            Console.WriteLine($"Kódolva: {kodoltpar}");
+
+            Console.ReadLine();
+        }
+    }
+}
